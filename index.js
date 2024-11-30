f39d3c5995a1b38543227072c07f51e12d709376
Santi <canosantiago404@gmail.com>
2024-11-30 11:34:43 -0300

    add multer a api post route

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
+// Middleware
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// Config multer
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Routes
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
-
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
